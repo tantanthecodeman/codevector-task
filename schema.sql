acdc68e73832e59b78ca1801b81e77b2eec259c9
Tanay Gujarathi <tanaygujarathig@gmail.com>
2026-06-24 14:19:50 +0530

    Automate updated_at tracking with database trigger

--- schema.sql
+++ schema.sql
@@ -3,11 +3,27 @@ CREATE TABLE IF NOT EXISTS products(
   name TEXT NOT NULL,
   category TEXT NOT NULL,
   price NUMERIC(10,2) NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFFAULT now(),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 
 );
 
 CREATE INDEX IF NOT EXISTS idx_products_created_id
-ON products(created_at DESC, id DESC);
+  ON products(created_at DESC, id DESC);
 
+CREATE INDEX IF NOT EXISTS idx_products_category_created_id
+  ON products(category, created_at DESC, id DESC);
+
+CREATE OR REPLACE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trg_products_updated_at ON products;
+CREATE TRIGGER trg_products_updated_at
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
